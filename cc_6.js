909167befad70649e3cc998e4f2b06e56800e08f
Giana Cresta <crestagiana@gmail.com>
2026-06-13 20:02:05 -0400

    manager subclass

--- cc_6.js
+++ cc_6.js
@@ -2,9 +2,20 @@
 class Employee { // blueprint for employee objects
     constructor(name, department) { // runs for each employee added/created
         this.name = name;
-        this.department = department; // store values (name, department) inside object
+        this.department = department; // store values (name, department) inside employee object
     }
     describe() {
-        return `${this.name} works in the ${this department} department.`;
+        return `${this.name} works in the ${this.department} department.`;
     } // uses template literals to output a sentence using the stored properties
+}
+
+// Manager Subclass
+class Manager extends Employee { // automatically inherits everything from Employee
+    constructor(name, department, teamSize) {
+        super(name, department); // calls the parent class (name, department)
+        this.teamSize = teamSize; // adding a new property
+    }
+    describe() {
+        return `${this.name} is the manager of the ${this.department} department and leads a team of ${this.teamSize}.`;
+    } // overriding the employee describe() method
 }
